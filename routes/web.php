4e158f120bbcd4c5580192e745d2e7fdbd9237c7
Emil <emil_vinther@hotmail.com>
2023-08-09 14:38:26 +0200

    Queries, Models and Eloquent

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,107 @@ use App\Http\Controllers\PostsController;
 |
 */
 
-Route::get('/', function () {
+
+Route::get('/findwhere', function(){
+    $posts = Post::where('id', 4)->orderBy('id', 'desc')->take(1)->get();
+
+    return $posts;
+
+});
+
+
+Route::get('/findmore', function(){
+     //$posts = Post::findOrFail(4);
+
+    $posts = Post::where('users_count', '<', 50)->firstOrFail();
+
+     return $posts;
+
+});
+//update and insert
+Route::get('/basicinsert', function(){
+    //$post = new Post;
+    $post = Post::find(2);
+
+    $post->title = "new ORM/ELOQUENT title updated ID 2";
+    $post->content = "Eleqoent is easy to update. This is so nice";
+
+    $post->save();
+
+});
+
+Route::get('/create', function(){
+    Post::create(['title'=>'The delete method', 'content'=>'We want to retrieve deleted data']);
+
+
+
+
+});
+
+Route::get('/update', function(){
+    Post::where('id', 2)->where('is_admin', 0)->update(['title'=>'NEW PHP ELOQUENT TITLE', 'content'=>'We are now using the update method with eloquent']);
+});
+
+Route::get('/delete', function(){
+
+    //Post::destroy([6,7,8,9,10,11,12,13]);
+
+
+    Post::destroy(13);
+    //Post::where('is_admin', 0)->delete();
+
+
+    //$post = Post::find(2);
+    //$post->delete();
+
+});
+
+Route::get('/readsoftdelete', function(){
+
+    //$post = Post::find(14);
+
+    //return $post;
+
+    //Include both trashed and non trashed items
+    //$post = Post::withTrashed()->where('id', 13)->get();
+    //return $post;
+
+    //Only Trashed items
+    $post = Post::onlyTrashed()->where('is_admin', 0)->get();
+    return $post;
+});
+
+Route::get('/restore', function (){
+    Post::withTrashed()->where('id', 13)->restore();
+});
+
+Route::get('/forcedelete', function (){
+
+    Post::withTrashed()->where('id', 13)->forceDelete();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Route::get('/', function () {
     return view('welcome');
 });
 
@@ -22,17 +123,10 @@ Route::get('/', function () {
 
 //Route::resource('posts', PostsController::class);
 
-Route::get('/contact', [PostsController::class, 'contact']);
+
+ Route::get('/contact', [PostsController::class, 'contact']);
 
 Route::get('/post/{id}/{name}/{password}', [PostsController::class, 'show_post']);
-
-
-
-
-
-
-
-/*
 Route::get('/about', function () {
     return "Here is about page";
 });
@@ -49,4 +143,52 @@ Route::get('/admin/posts/example', array('as'=>'admin.home',function() {
    $url = route('admin.home');
    return "This url is ". $url;
 }));
+
+Route::get('/insert', function(){
+    DB::insert('insert into posts(title, content) values(?, ?)', ['PHP with Laravel', 'This is the content for the post with the title PHP with Laravel']);
+
+});
+
+Route::get('/read', function(){
+
+    $results = DB::select('select * from posts where id = ?', [1]);
+    foreach ($results as $post){
+        return $post->title;
+    }
+});
+
+Route::get('/update', function(){
+    $updated = DB::update('update posts set title = "Updated title" where id = ?', [1]);
+
+    return $updated;
+
+});
+
+Route::get('/delete', function(){
+    $deleted = DB::delete('delete from posts where id = ?', [1]);
+
+    return $deleted;
+});
+
+
+Route::get('/find', function () {
+
+    $posts = Post::all();
+
+    foreach ($posts as $post) {
+        return $post->title;
+
+    }
+
+
+
+
+
+});
+
+Route::get('/find', function() {
+    $post = Post::find(2);
+
+    return $post->title;
+});
 */
